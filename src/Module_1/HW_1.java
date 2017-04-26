9dd5c8f5fd2293a01aae67b50e0a7d4b51b33413
DenisRaochiy <denisdzerg@gmail.com>
2017-04-26 14:03:08 +0300

    test2

---
+++ src/Module_1/HW_1.java
@@ -0,0 +1,15 @@
+package Module_1;
+
+import java.util.Scanner;
+
+/**
+ * Created by denis on 26.04.2017.
+ */
+public class HW_1 {
+    public static void main(String[] args) {
+        System.out.println("Enter your name");
+        Scanner scanner = new Scanner (System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
