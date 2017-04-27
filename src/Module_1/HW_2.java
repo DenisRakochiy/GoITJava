4a92cf1642488c1f2a57f51be0d9e12601c138d3
DenisRaochiy <denisdzerg@gmail.com>
2017-04-27 14:12:43 +0300

    add HW_2

--- src/Module_1/HW_2.java
+++ src/Module_1/HW_2.java
@@ -1,11 +1,24 @@
 package Module_1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by denis on 26.04.2017.
  */
-// gggdgdgd
+
 public class HW_2 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int count = 5;
+        int[] arr = new int[count];
+            for (int i=0; i<5; i++){
+                System.out.println("Enter number "+(i+1));
+                arr[i] = sc.nextInt();
+            }
+            System.out.println("Entered array " + Arrays.toString(arr));
+            Arrays.sort(arr);
+            System.out.println("Sorted array " + Arrays.toString(arr));
     }
 }
+
